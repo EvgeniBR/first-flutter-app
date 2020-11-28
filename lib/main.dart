3246a912ce433fc1782184ac653d21874eb53576
EvgeniBR <genib1989@gmail.com>
2020-11-28 17:59:36 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:first_app/result.dart';
+import 'package:flutter/material.dart';
+import './quiz.dart';
+import './result.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'what\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'Whie', 'score': 1}
+      ],
+    },
+    {
+      'questionText': 'what\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9}
+      ],
+    },
+    {
+      'questionText': 'who\'s your favorite Band?',
+      'answers': [
+        {'text': 'Europe', 'score': 4},
+        {'text': 'Queen', 'score': 1},
+        {'text': 'Puddle of Mud', 'score': 10},
+        {'text': 'Seather', 'score': 7}
+      ],
+    },
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('My first App'),
+      ),
+      body: _questionIndex < _questions.length
+          ? Quiz(
+              answerQuestion: _answerQuestion,
+              questions: _questions,
+              questionIndex: _questionIndex,
+            )
+          : Result(_totalScore, _resetQuiz),
+    ));
+  }
+}
